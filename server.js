aff13b483e25ee916586e26662221e03c813387f
lalithamadhaviM <lalithamadhavi.mangina@sky.uk>
2024-09-16 08:02:50 +0530

    Adding an about page

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
   res.send('Hello, this is a collaborative GitHub project!');
 });
 
+// About page route
+app.get('/about', (req, res) => {
+  res.send('<h1>About Us</h1><p>This is the About page of our web application.</p>');
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
